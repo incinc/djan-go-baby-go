3271be39f6dbebcbedd19037a7a03dfd5f691a2c
Adrien Delessert <adrien@delessert.com>
2024-01-13 19:28:52 -0500

    Fully remove directory

--- testapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class gobabygoConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "gobabygo"
--- testapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'index.jinja2')
-
-
-def vue_index(request):
-    return render(request, "vue_base.jinja2")
