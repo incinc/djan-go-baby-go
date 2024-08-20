deb0512571344d3032fef2caf6fffea6def2eab4
Adrien Delessert <adrien@delessert.com>
2024-08-20 15:43:45 -0400

    Add basic admin with default models

--- gobabygo/admin.py
+++ gobabygo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from gobabygo.models import User, Profile
+
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user"]
